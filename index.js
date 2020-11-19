b6e0f65d31fe493941444f76b07ccc16c78ae4e9
JuanCarlos OrtizPinto <juancarlos_tecno91@hotmail.com>
2020-11-19 03:57:11 -0600

    update

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ require('dotenv').config();
 const app = express();
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use(express.static(path.join(__dirname,'dist','la-vaca-voladora')));
+app.use(express.static(path.join(__dirname,'dist')));
 app.use('/api', cors());
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname,'/dist','la-vaca-voladora','index.html'));
+    res.sendFile(path.join(__dirname,'/dist','index.html'));
     });
 apiRoutes(app);
 
