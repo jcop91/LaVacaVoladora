b2f9614b798b6048d0d6c4c824253a2280705457
JuanCarlos OrtizPinto <juancarlos_tecno91@hotmail.com>
2020-11-19 05:04:06 -0600

    g

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4200;
 const path = require('path');
 const cors = require('cors');
 
@@ -18,7 +18,8 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.static(path.join(__dirname,'dist','la-vaca-voladora')));
 app.use('/api', cors());
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname,'/dist','la-vaca-voladora','index.html'));
+    res.sendFile(path.join(__dirname,'/dist/la-vaca-voladora/index.html'));
+    //res.sendFile(path.join(__dirname,'/public/index.html'));
     });
 apiRoutes(app);
 
